899122702a63400eeccd43ed46938a93f441d184
15039836 <15039836@myrp.edu.sg>
2017-08-08 14:52:08 +0800

    bio

--- app/src/main/java/p12mydataboook/android/myapplicationdev/com/p12mydatabook/BioFragment.java
+++ app/src/main/java/p12mydataboook/android/myapplicationdev/com/p12mydatabook/BioFragment.java
@@ -1,11 +1,18 @@
 package p12mydataboook.android.myapplicationdev.com.p12mydatabook;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 /**
@@ -13,6 +20,8 @@ import android.view.ViewGroup;
  */
 public class BioFragment extends Fragment {
 
+    TextView tvBio;
+    Button btnEdit;
 
     public BioFragment() {
         // Required empty public constructor
@@ -23,7 +32,40 @@ public class BioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.biofragment, container, false);
+        View view =  inflater.inflate(R.layout.biofragment, container, false);
+
+        tvBio = (TextView) view.findViewById(R.id.tvBio);
+        btnEdit = (Button) view.findViewById(R.id.btnEdit);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                final LinearLayout passPhrase = (LinearLayout) inflater.inflate(R.layout.bio, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Edit Bio")
+                        .setView(passPhrase)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                EditText rg1 = (EditText) passPhrase.findViewById(R.id.etBio);
+
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+        return view;
     }
 
 }
